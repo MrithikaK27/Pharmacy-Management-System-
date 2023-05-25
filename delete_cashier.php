ff9481c3aa1647f5bbe24e459f7a29163e193fc0
Mrithika2803 <mrithika27nov@gmail.com>
2023-05-25 21:27:43 +0530

    first commit

---
+++ delete_cashier.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include_once('connect_db.php');
+if(isset($_SESSION['username'])){
+$id=$_SESSION['admin_id'];
+$user=$_SESSION['username'];
+}else{
+header("location:http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/index.php");
+exit();
+}
+$id=$_GET['cashier_id'];
+$sql=mysqli_query($conn, "delete from cashier where cashier_id='$id'");
+if($sql){
+//$rows=mysql_fetch_assoc($result);
+header("location:admin_cashier.php");
+}
+else{
+    echo'<script>wimdow.alert("Failed!!! Please try again")</script>';
+}
+?>
+
+
